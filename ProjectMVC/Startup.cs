15b892eb06bf5f0fe08aa138789b6bfa64394eb0
ahmedramadan11 <42189675+ahmedramadan11@users.noreply.github.com>
2022-03-24 23:33:55 +0200

    b7b ya hussein

--- ProjectMVC/Startup.cs
+++ ProjectMVC/Startup.cs
@@ -14,6 +14,9 @@ using System.Threading.Tasks;
 
 namespace ProjectMVC
 {
+    /// <summary>
+    /// comment
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
