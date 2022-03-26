8766b3c7b99b788e146fdd7dca045c87d0267413
ahmedramadan11 <42189675+ahmedramadan11@users.noreply.github.com>
2022-03-27 00:43:28 +0200

    Add admin functions

--- ProjectMVC/Models/Category.cs
+++ ProjectMVC/Models/Category.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectMVC.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         public ICollection <Product> Products { get; set; }
 
---
+++ ProjectMVC/Services/ProductRepoService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectMVC.Services
+{
+    public class ProductRepoService : IProductRepository
+    {
+        public ProductRepoService(ShopDBContext context) {
+            Context = context;
+        }
+
+        public ShopDBContext Context { get; }
+
+        public void Delete(int id)
+        {
+            Context.products.Remove(Context.products.Find(id));
+            Context.SaveChanges();
+        }
+
+        public List<Product> GetAll()
+        {
+            return Context.products.Include(p=>p.Category).ToList();
+        }
+
+        public Product GetDetails(int id)
+        {
+            return Context.products.Include(p => p.Category).FirstOrDefault(p=>p.Id==id);
+        }
+
+        public void Insert(Product product)
+        {
+            Context.products.Add(product);
+            Context.SaveChanges();
+        }
+
+        public void Update(int id, Product product)
+        {
+            Product currProduct = Context.products.Find(id);
+            currProduct.Name = product.Name;
+            currProduct.Description=product.Description;
+            currProduct.Price= product.Price;
+            currProduct.NumInStock = product.NumInStock;
+            currProduct.ImgPath = product.ImgPath;
+            currProduct.CategoryId= product.CategoryId; 
+            Context.SaveChanges();
+
+        }
+    }
+}
