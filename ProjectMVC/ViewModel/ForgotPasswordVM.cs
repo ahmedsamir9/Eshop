58180bace70185c0add56cd8c4a0cd7446984ad0
Mohamed Hussein <Mohamed Hussein@DESKTOP-LD598HV>
2022-04-05 23:14:44 +0200

    Navbar

---
+++ ProjectMVC/ViewModel/ForgotPasswordVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectMVC.ViewModel
+{
+    public class ForgotPasswordVM
+    {[Required]
+    [EmailAddress]
+     public string Email { get; set; }   
+
+    }
+}
